beb7260416495ecdfd30c2ba2dc7eb3ea023cbe8
Rainiero1000 <mantillacalderonedmund@gmail.com>
2026-02-01 03:01:57 -0500

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,14 +60,12 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Edmund Mantilla.</h2>;
+  const three = <h3 className="big-heading">I am an information technology specialist.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
+        I am a business administrator passionate about finance, investments, and technology.{' '}
         <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
           Upstatement
         </a>
@@ -78,10 +76,10 @@ const Hero = () => {
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href="mantillacalderonedmund@gmail.com"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      ¡Contáctame!
     </a>
   );
 
